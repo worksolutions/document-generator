a63362eaa472f046fe7b9127b0126171f2a80ed3
Smotrov Dmitriy <dsxack@gmail.com>
2015-03-25 10:32:42 +0300

    Добавлена возможность работы с таблицами

--- src/WordTemplateProcessor.php
+++ src/WordTemplateProcessor.php
@@ -10,24 +10,21 @@ use PhpOffice\PhpWord\TemplateProcessor as PhpOfficeTemplateProcessor;
 
 class WordTemplateProcessor extends AbstractTemplateProcessor {
 
-    private $_fields = [];
+    private $_values = [];
+    private $_rows = [];
     /** @var PhpOfficeTemplateProcessor */
     private $_processor;
 
     public function process() {
-        $processor = new PhpOfficeTemplateProcessor($this->getTemplate());
-
-        foreach ($this->_fields as $name => $value) {
-            $processor->setValue($name, $value);
-        }
-
-        $this->_processor = $processor;
+        $this->_setProcessor(new PhpOfficeTemplateProcessor($this->getTemplate()));
+        $this->_processValues();
+        $this->_processTableRows();
 
         return $this;
     }
 
-    public function setField($name, $value) {
-        $this->_fields[$name] = $value;
+    public function setValue($name, $value) {
+        $this->_values[$name] = $value;
 
         return $this;
     }
@@ -36,5 +33,35 @@ class WordTemplateProcessor extends AbstractTemplateProcessor {
         $this->_processor->saveAs($outputPath);
 
         return $this;
+    }
+
+    public function addTableRow($blockWithClonedRow, $item) {
+        $this->_rows[$blockWithClonedRow][] = $item;
+    }
+
+    private function _getProcessor() {
+        return $this->_processor;
+    }
+
+    private function _processValues() {
+        foreach ($this->_values as $name => $value) {
+            $this->_getProcessor()->setValue($name, $value);
+        }
+    }
+
+    private function _processTableRows() {
+        foreach ($this->_rows as $rowName => $rows) {
+            $this->_getProcessor()->cloneRow($rowName, count($rows));
+
+            foreach ($rows as $cnt => $row) {
+                foreach ($row as $name => $value) {
+                    $this->_getProcessor()->setValue($name . '#' . ($cnt + 1), $value);
+                }
+            }
+        }
+    }
+
+    private function _setProcessor($processor) {
+        $this->_processor = $processor;
     }
 }
--- tests/units/WordTemplateProcessorTest.php
+++ tests/units/WordTemplateProcessorTest.php
@@ -12,13 +12,47 @@ use WS\DocumentGenerator\WordTemplateProcessor;
 class WordTemplateProcessorTest extends PHPUnit_Framework_TestCase {
     public function testGenerate(){
         $processor = new WordTemplateProcessor();
-
         $outputPath = tempnam(sys_get_temp_dir(), '');
 
+        $processor
+            ->setValue('company_name', 'WorkSolutions')
+            ->setValue('name', 'Smotrov Dmitriy')
+            ->setValue('address', 'Lenina 13')
+            ->setValue('city', 'Rostov-on-Don')
+            ->setValue('state', 'Rostovskaya oblast')
+            ->setValue('zip', '344000')
+            ->setValue('date', (new \DateTime())->format('Y-m-d'))
+            ->setValue('project_title', 'PhpWord project')
+            ->setValue('project_description', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex illum inventore omnis sequi suscipit. Aspernatur aut doloribus earum molestias numquam, odio optio quibusdam reiciendis sed similique sint sunt ullam vero?')
+            ->setValue('invoice_number', 21);
+
+        $items = [
+            [
+                'product_title' => 'Test product #1',
+                'product_quantity' => $quantity = 2,
+                'product_price' => $price = 1000,
+                'product_cost' => $quantity * $price
+            ],
+            [
+                'product_title' => 'Test product #2',
+                'product_quantity' => $quantity = 5,
+                'product_price' => $price = 1500,
+                'product_cost' => $quantity * $price
+            ],
+        ];
+
+        $total = 0;
+        foreach ($items as $item) {
+            $total += $item['product_cost'];
+            $processor->addTableRow('product_title', $item);
+        }
+
+        $processor->setValue('total', $total);
+
         $processor->useTemplate(__DIR__ . "/../data/WordTemplateProcessorTest/testGenerate/template.docx")
-            ->setField('COMPANY_NAME', 'WorkSolutions')
             ->process()
             ->saveAs($outputPath);
+
 
         $matcher = new WordDocumentsMatcher();
         $isMatch = $matcher->isMatch($outputPath, __DIR__ . '/../data/WordTemplateProcessorTest/testGenerate/result.docx');
